384c3741ff4a46ef13757295d2651adf8d05472d
Rich Berrill <rberrill@gmail.com>
2011-09-25 16:47:32 -0500

    Cleaned up code formatting for the widgets

--- widget-faq.php
+++ widget-faq.php
@@ -1,11 +1,11 @@
-            <?php $page_id = get_the_ID(); ?>
-            <?php if(get_field('faq',$page_id)) { ?>
-            <ul>
-		    	<?php while(the_repeater_field('faq',$page_id)) { ?>
-				<li style="margin-bottom: 10px;">
-					<h5><?php the_sub_field('question'); ?></h5>
-					<span style="font-style:italic;font-size:.75em;"><?php the_sub_field('information'); ?></span>
-				</li>
-				<?php } ?>
-    		<ul>
-    		<?php } ?>
+<?php $page_id = get_the_ID(); ?>
+<?php if (get_field('faq', $page_id)) { ?>
+    <ul>
+        <?php while (the_repeater_field('faq', $page_id)) { ?>
+            <li style="margin-bottom: 10px;">
+                <h5><?php the_sub_field('question'); ?></h5>
+                <span style="font-style:italic;font-size:.75em;"><?php the_sub_field('information'); ?></span>
+            </li>
+        <?php } ?>
+        <ul>
+        <?php } ?>
--- widget-testimonials.php
+++ widget-testimonials.php
@@ -1,21 +1,21 @@
 <?php
 global $post;
-	$tmp_post = $post;
-    $args = array( 'post_type' => 'testimonials', 'posts_per_page' => 1, 'orderby' => 'rand' );
+$tmp_post = $post;
+$args = array('post_type' => 'testimonials', 'posts_per_page' => 1, 'orderby' => 'rand');
 $myposts = get_posts($args);
 ?>
 <div id="testimonials">
-<?php
-foreach( $myposts as $post ) {
-setup_postdata($post);
-?><blockquote><?php
-the_field("testimonial_comment");
-?></blockquote><?php
-?><p><?php
-the_field("testimonial_name");
-?></p><?php
-}
-?>
+    <?php
+    foreach ($myposts as $post) {
+        setup_postdata($post);
+        ?><blockquote><?php
+    the_field("testimonial_comment");
+        ?></blockquote><?php
+        ?><p><?php
+    the_field("testimonial_name");
+        ?></p><?php
+    }
+    ?>
 </div>
 <?php
 $post = $tmp_post;
